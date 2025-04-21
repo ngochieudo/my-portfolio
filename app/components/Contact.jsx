43b4e0871cc24ac861dd4da9b6ef6e0651345526
Hieu Do <hieudngch190863@fpt.edu.vn>
2025-04-22 04:43:48 +0700

    change portfolio data

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,10 +1,9 @@
-import { Button, Divider, Tooltip, Typography } from "@mui/material";
+import { Tooltip, Typography } from "@mui/material";
 import React from "react";
 
 import EmailIcon from "@mui/icons-material/Email";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 const Contact = () => {
   return (
@@ -29,9 +28,8 @@ const Contact = () => {
       >
         Here are my contact information
       </Typography>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 p-6 rounded-2xl mx-auto w-10/12">
         <div className="flex-col justify-center items-center">
-          <div className="rounded-xl overflow-hidden">
+          <div className="overflow-hidden">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.3880375918743!2d105.83115597625084!3d21.01715418062923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab83907b09d5%3A0x31426b71de4e69e6!2zMjQgTmdoLiA3OSBOZy4gVGjhu5UgUXVhbiwgVGjhu5UgUXVhbiwgxJDhu5FuZyDEkGEsIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1745237547007!5m2!1svi!2s"
               width="100%"
@@ -41,7 +39,7 @@ const Contact = () => {
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
           </div>
-          <div className="space-y-2 text-white mt-5">
+          <div className="space-y-2 text-white mt-5 flex flex-col items-center">
             <div>
               <Tooltip title="Email of Hieu Do" arrow placement="top">
                 <a href="mailto:ngochieudo2001@gmail.com">
@@ -85,51 +83,6 @@ const Contact = () => {
             </div>
           </div>
         </div>
-
-        <form className="space-y-4">
-          <input
-            type="text"
-            placeholder="Your Name"
-            className="w-full border rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <input
-            type="email"
-            placeholder="Your Email"
-            className="w-full border rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <input
-            type="text"
-            placeholder="Subject"
-            className="w-full border rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <textarea
-            rows="5"
-            placeholder="Your Message"
-            className="w-full border rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          ></textarea>
-          <Button
-            variant="outlined"
-            size="large"
-            href="#contact"
-            sx={{
-              marginTop: "20px",
-              color: "white",
-              borderColor: "white",
-              "&:hover": {
-                backgroundColor: "white",
-                color: "black",
-              },
-              textTransform: "none",
-              fontSize: "16px",
-              fontWeight: "bold",
-              textAlign: "center",
-              width: "100%",
-            }}
-          >
-            Send Message <ChevronRightIcon />
-          </Button>
-        </form>
-      </div>
     </>
   );
 };
--- app/data/index.js
+++ app/data/index.js
@@ -24,34 +24,17 @@ export const projects = [
 ];
 
 
-
-export const companies = [
-  {
-    id: 1,
-    name: "FPT Software",
-    img: "/fsoft.svg",
-    nameImg: "/cloudName.svg",
-  },
-
-];
-
-export const workExperience = [
-  {
-    id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using HTML/CSS, JavaScript and Angular.",
-    className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
-  },
-];
-
 export const timelineData = [
   {
-    title: "2024",
+    title: "Early 2025",
     content: (
       <div>
         <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
-          Built and launched Aceternity UI and Aceternity UI Pro from scratch
+          Finished the OJT training as developer intern at FPT Software.
+        </p>
+        <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+          At FPT Software, I am working on how to build a web application using JavaScript language. 
+          I am also learning how to use the latest technologies and frameworks to build modern web applications like Nest.js and Angular. 
         </p>
         <div className="grid grid-cols-2 gap-4">
           <img
@@ -87,7 +70,7 @@ export const timelineData = [
     ),
   },
   {
-    title: "Early 2023",
+    title: "2024",
     content: (
       <div>
         <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
@@ -98,60 +81,6 @@ export const timelineData = [
           Lorem ipsum is for people who are too lazy to write copy. But we are
           not. Here are some more example of beautiful designs I built.
         </p>
-        <div className="grid grid-cols-2 gap-4">
-          <img
-            src="https://assets.aceternity.com/pro/hero-sections.png"
-            alt="hero template"
-            width={500}
-            height={500}
-            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-          />
-          <img
-            src="https://assets.aceternity.com/features-section.png"
-            alt="feature template"
-            width={500}
-            height={500}
-            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-          />
-          <img
-            src="https://assets.aceternity.com/pro/bento-grids.png"
-            alt="bento template"
-            width={500}
-            height={500}
-            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-          />
-          <img
-            src="https://assets.aceternity.com/cards.png"
-            alt="cards template"
-            width={500}
-            height={500}
-            className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-          />
-        </div>
-      </div>
-    ),
-  },
-  {
-    title: "Changelog",
-    content: (
-      <div>
-        <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
-          Deployed 5 new components on Aceternity today
-        </p>
-        <div className="mb-8">
-          <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-            ✅ Card grid component
-          </div>
-          <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-            ✅ Startup template Aceternity
-          </div>
-          <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-            ✅ Random file upload lol
-          </div>
-          <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
-            ✅ Himesh Reshammiya Music CD
-          </div>
-        </div>
         <div className="grid grid-cols-2 gap-4">
           <img
             src="https://assets.aceternity.com/pro/hero-sections.png"
